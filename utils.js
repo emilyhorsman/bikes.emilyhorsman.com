5388e3714fa415c105ae99b3e467fd9f1d4d5c6b
Emily Horsman <me@emilyhorsman.com>
2024-01-14 09:13:06 -0800

    reset avif options
    [ci skip]

--- utils.js
+++ utils.js
@@ -30,7 +30,7 @@ function getImageFilename(src, width, format) {
 export function getSharpOptions(data) {
   const options = {
     concurrency: 20,
-    sharpAvifOptions: { quality: 45, effort: 6 },
+    sharpAvifOptions: {},
     filenameFormat: function (_, src, width, format) {
       return getImageFilename(src, width, format);
     },
